23e6b31efabedecbf8c720a9f6e6619a38e83e5f
Yoshiki Hemmi <yoshiki.hemmi@dena.com>
2022-10-11 12:14:48 +0900

    Initial Commit.

---
+++ Package/Sources/App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SPMPlugin
+//
+//  Created by Yoshiki Hemmi on 2022/10/11.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+    
+    public init() {}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPMPlugin/SPMPluginApp.swift
@@ -0,0 +1,18 @@
+//
+//  SPMPluginApp.swift
+//  SPMPlugin
+//
+//  Created by Yoshiki Hemmi on 2022/10/11.
+//
+
+import SwiftUI
+import App
+
+@main
+struct SPMPluginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
